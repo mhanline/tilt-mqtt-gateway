94403bf60d5b42f0371c37cfca2c2c0251ef4642
mhanline <30210174+mhanline@users.noreply.github.com>
2020-04-27 22:45:16 +1000

    Fixes #1, Fixes #2, Fixes #21. - Now sends unixtime timestamp as part of the protobuf message - Now using the BLEAdvertisedDeviceCallbacks callback to simplify things, and less looping - Removed most Strings and do binary comparisons on the payload to identify a Tilt (should be less memory) - Made the Temp and SG messages unsigned int, which are what we received the message from the Tilt in anyway - Added future support in the protobuf message for TxPower incase it's needed - Implemented a message version in case I want to change the protocol in the future

--- src/main.cpp
+++ src/main.cpp
@@ -14,50 +14,131 @@ Publishes SG and Temp readings to Google Cloud IoT Core via MQTT
 
 // User Settings
 
-#define bleScanTime (int)5 // Duration to scan for bluetooth devices in seconds
-#define publishTime (int)3570000 // PubSub publish interval (in ms)
+#define bleScanTime (int)10 // Duration to scan for bluetooth devices in seconds
+#define publishTime (int)1800000 // 30 minutes: PubSub publish interval (in ms)
 #define Celsius true
 #define TRIGGER_PIN 16 // AP factory reset button
+#define PROTOMSGVER 1 //Bump this if changing the protobuf format
+#define scanTime 10 //Time to run the BT scan in seconds
 
-int repeatColour = 0;
 unsigned long lastMillis = 0;
 BLEScan* pBLEScan;
 
-int parseTilt(String inputData, float *inputTemp, float *inputGravity, uint8_t *inputColourNum) {
-  // Determine the Colour
-  *inputColourNum = inputData.substring(6, 7).toInt();
-  // Get the temperature
-  *inputTemp = strtol(inputData.substring(32, 36).c_str(), NULL, 16);
-  // Get the gravity
-  *inputGravity = strtol(inputData.substring(36, 40).c_str(), NULL, 16) / 1000;
-  DPRINTF("Device Colour detected: ");
-  DPRINTLN(*inputColourNum);
-  DPRINTF("Temp: ");
-  #ifdef Celsius
-  *inputTemp = (*inputTemp-32.0) * (5.0/9.0);
-  DPRINT(*inputTemp);
-  DPRINTLNF(" C");
-  #else
-  DPRINT(*inputTemp);
-  DPRINTLNF(" F");
-  #endif
-  DPRINTF("Gravity: ");
-  DPRINTLN(*inputGravity, 3);
-  DEBUG_PRINTLN(inputData);
-  return 2;
-}
-
 WiFiManager wm;
 
-void checkButton(){
+void printhex(const uint8_t *readmem, const unsigned int memsize);
+
+void checkButton();
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      uint8_t mfgData[51] = {}; //To-Do: reduce this size if necessary
+      unsigned int payloadLen = advertisedDevice.getPayloadLength();
+      memcpy(mfgData, advertisedDevice.getPayload() + 2, payloadLen);
+      /*DPRINTF("MFG STRING: ");
+      printhex(mfgData, payloadLen);*/
+      if ((memcmp(mfgData, "\x4c\x00\x02\x15\xa4\x95\xbb", 7) != 0) && 
+      (memcmp(mfgData + 8, "\xc5\xb1\x4b\x44\xb5\x12\x13\x70\xf0\x2d\x74\xde", 12) != 0)) {
+        return;
+      }
+      DPRINTLNF("Found a tilt!");
+      // To-Do: Validate tilt colour field
+      char tiltColourNum[1];
+      memcpy(tiltColourNum, mfgData + 7, sizeof(tiltColourNum));
+      DPRINTFMT("Tilt Colour shift 4: %d\n", (int)tiltColourNum[0] >> 4);
+      uint16_t tiltTemp;
+      memcpy(&tiltTemp, mfgData + 20, sizeof(tiltTemp));
+      tiltTemp = __builtin_bswap16(tiltTemp);
+      DPRINTFMT("Temperature in deg F: %d\n", (int)tiltTemp);
+      uint16_t tiltSG;
+      memcpy(&tiltSG, mfgData + 22, sizeof(tiltSG));
+      tiltSG = __builtin_bswap16(tiltSG);
+      DPRINTFMT("Specific Gravity (float): %.3f\n", (float)tiltSG / 1000);
+      bool status;
+      uint8_t msgPayload[50];
+      tiltmsg message = tiltmsg_init_zero;
+      pb_ostream_t stream = pb_ostream_from_buffer(msgPayload, sizeof(msgPayload));
+      time_t unixTNow;
+      time(&unixTNow);
+      message.version = PROTOMSGVER;
+      message.specificGravity = (int)tiltSG;
+      message.temperature = (int)tiltTemp;
+      //If we shift 4 bytes right, we get the correct decimal num
+      message.colour = tiltmsg_colour_type(tiltColourNum[0] >> 4);
+      message.timeStamp = unixTNow;
+      message.txPower = 1; //To-Do: Add TxPower. Enable in in debug mode.
+      status = pb_encode(&stream, tiltmsg_fields, &message);
+      DPRINTFMT("Bytes Written: %d \n", stream.bytes_written);
+      DPRINTF("Protobuf Message: ");
+      printhex(msgPayload, (unsigned int)(stream.bytes_written));
+      if (!status) {
+        DPRINTFMT("Encoding failed: %s\n", PB_GET_ERROR(&stream));
+      }
+      publishTelemetry((const char *)msgPayload, stream.bytes_written);
+    }
+};
+
+void setup() {
+  pinMode(TRIGGER_PIN, INPUT_PULLUP);
+  WiFi.mode(WIFI_STA);
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+
+  bool res;
+  res = wm.autoConnect("tilt-gw-init","tilt-gateway"); // password protected ap
+  if(!res) {
+      DPRINTLNF("Failed to connect");
+      // ESP.restart();
+  } 
+  else { 
+      DPRINTF("WiFi Connected...)");
+  }
+  setupCloudIoT();
+  lastMillis = millis() + publishTime; // Starts an initial publish straight away
+  BLEDevice::init("");
+}
+
+void loop() {
+  checkButton();
+  if (!mqttClient->connected()) {
+    connect();
+  }
+  mqtt->loop();
+  if ((millis() - lastMillis) > publishTime) {
+    lastMillis = millis();
+    pBLEScan = BLEDevice::getScan();
+    MyAdvertisedDeviceCallbacks myCallbacks;
+    pBLEScan->setAdvertisedDeviceCallbacks(&myCallbacks);
+    pBLEScan->setActiveScan(true);
+    pBLEScan->setInterval(40);
+    pBLEScan->setWindow(30);
+    BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+    DPRINTF("Devices found: ");
+    DPRINTLN(foundDevices.getCount());
+    DPRINTLNF("Scan done!");
+    pBLEScan->clearResults();
+  }
+}
+
+void printhex(const uint8_t *readmem, const unsigned int memsize) {
+  if (memsize > 1) {
+    DPRINTFMT("%02X", readmem[0]);
+  }
+  for (int i = 1; i < memsize; i++) {
+    DPRINTFMT("-%02X", readmem[i]);
+  }
+  DPRINTLNF("");
+}
+
+void checkButton() {
   if ( digitalRead(TRIGGER_PIN) == LOW ) {
     // poor mans debounce/press-hold, code not ideal for production
     delay(50);
-    if(digitalRead(TRIGGER_PIN) == LOW ){
+    if(digitalRead(TRIGGER_PIN) == LOW ) {
       DPRINTLNF("Button Pressed");
       // still holding button for 3000 ms, reset settings
       delay(3000); // reset delay hold
-      if(digitalRead(TRIGGER_PIN) == LOW ){
+      if(digitalRead(TRIGGER_PIN) == LOW ) {
         DPRINTLNF("Button Held");
         DPRINTLNF("Erasing Config, restarting");
         wm.resetSettings();
@@ -72,103 +153,11 @@ void checkButton(){
         DPRINTLNF("failed to connect or hit timeout");
         delay(3000);
         // ESP.restart();
-      } else {
+      }
+      else {
         //if you get here you have connected to the WiFi
         DPRINTLNF("connected...yeey :)");
       }
-    }
-  }
-}
-
-void setup() {
-  pinMode(TRIGGER_PIN, INPUT_PULLUP);
-  WiFi.mode(WIFI_STA);
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-
-  bool res;
-  res = wm.autoConnect("tilt-gw-init","tilt-gateway"); // password protected ap
-  if(!res) {
-      Serial.println("Failed to connect");
-      // ESP.restart();
-  } 
-  else { 
-      Serial.println("WiFi Connected...)");
-  }
-  setupCloudIoT();
-  lastMillis = millis() + publishTime; // Starts an initial publish straight away
-}
-
-void loop() {
-  checkButton();
-  if (!mqttClient->connected()) {
-    connect();
-  }
-  mqtt->loop();
-  // delay(10);  // <- fixes some issues with WiFi stability
-  if ((millis() - lastMillis) > publishTime) {
-    lastMillis = millis();
-    int deviceCount, tiltCount = 0;
-    int colourFound = 1;
-    BLEDevice::init("");
-    pBLEScan = BLEDevice::getScan();
-    pBLEScan->setActiveScan(true);
-    BLEScanResults foundDevices = pBLEScan->start(bleScanTime);
-    deviceCount = foundDevices.getCount();
-    DPRINT(deviceCount);
-    DPRINTLNF(" Devices Found.");
-    pBLEScan->stop();
-    BLEDevice::deinit(0);
-    for (uint32_t i = 0; i < deviceCount; i++) {
-      BLEAdvertisedDevice Device = foundDevices.getDevice(i);
-      String DevString;
-      String DevData;
-      DevString = Device.toString().c_str();
-      DevData = DevString.substring(63);
-      if (DevData.substring(7, 32) == "0c5b14b44b5121370f02d74de") { // Tilt found
-        tiltCount++;
-        DPRINTF("Device #");
-        DPRINT(i);
-        DPRINTLNF(" is a Tilt");
-        float DevTemp;
-        float DevGravity;
-        uint8_t devColourNum;
-        int tiltSuccess = parseTilt(DevData, &DevTemp, &DevGravity, &devColourNum);
-        bool status;
-        uint8_t strPayload[120];
-        tiltmsg message = tiltmsg_init_zero;
-        pb_ostream_t stream = pb_ostream_from_buffer(strPayload, sizeof(strPayload));
-        time_t unixTNow;
-        time(&unixTNow);
-        message.specificGravity = DevGravity;
-        message.temperature = DevTemp;
-        message.colour = tiltmsg_colour_type(devColourNum);
-        message.timeStamp = unixTNow;
-        status = pb_encode(&stream, tiltmsg_fields, &message);
-        DPRINTF("Bytes Written: ");
-        DPRINTLN(stream.bytes_written);
-        DPRINTF("Message: ");
-        for(int i = 0; i<stream.bytes_written; i++){
-          DPRINTFMT("%02X",strPayload[i]);
-        }
-        if (!status) {
-          printf("Encoding failed: %s\n", PB_GET_ERROR(&stream));
-        }
-        publishTelemetry((const char *)strPayload, sizeof(strPayload));
-        // DEBUG_PRINTLN(strPayload);
-        if (tiltSuccess == 2) {
-          colourFound = 2;
-        }
-        if (!tiltSuccess && colourFound != 2) {
-          colourFound = 0;
-        }
-      }
-    }
-    if (!tiltCount || !colourFound) {
-      DPRINTLNF("No Tilts Found.");
-    }
-    else {
-
     }
   }
 }
